20ae147946c4717fa5bf4d9a82f9cda1b838c1f2
JoseMagg <jose.gonzalezg1@udea.edu.co>
2023-10-23 12:56:20 -0500

    Movimiento de pedestales

--- Assets/Game/Scripts/Movement/PedestalsMovement.cs
+++ Assets/Game/Scripts/Movement/PedestalsMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,14 +7,30 @@ using UnityEngine.EventSystems;
 public class PedestalsMovement : MonoBehaviour
 {
     public List<Transform> pedestals = new List<Transform>();
-    private Transform nextPedestal;
+    public float betweenDistance;
     public Transform cameraTransform;
-    private int currentPedestal = -1;
-
     public ScenarioConfig config;
+    public AnimationClip startAnimation;
+
+    private Transform nextPedestal;
+    private int currentPedestal = -1;
+    private Transform lastPedestal;
+
     void Start()
     {
-        Move();
+        Pause();
+        PlacePedestals();
+    }
+
+    private void PlacePedestals()
+    {
+        for (int i = 0; i < pedestals.Count; i++)
+        {
+            Vector3 position = pedestals[i].position;
+            position.z = betweenDistance * (i + 1);
+            pedestals[i].position = position;
+        }
+
     }
 
     void Update()
@@ -21,22 +38,33 @@ public class PedestalsMovement : MonoBehaviour
         if (config.isMoving)
         {
             transform.Translate(Vector3.Normalize(config.moveDirection) * config.speed * Time.deltaTime);
+            if (Mathf.Abs(cameraTransform.position.z - nextPedestal.position.z) < config.stopDistance)
+            {
+                Pause();
+            }
         }
-        if (Mathf.Abs(cameraTransform.position.z - nextPedestal.position.z) < config.stopDistance)
+        if (lastPedestal != null)
         {
-            Pause();
+            if (cameraTransform.position.z - lastPedestal.position.z > config.stopDistance)
+            {
+                Vector3 position = lastPedestal.localPosition;
+                position.z += betweenDistance * 2;
+                lastPedestal.localPosition = position;
+            }
         }
     }
 
     private bool NextPedestal()
     {
-        if (currentPedestal < pedestals.Count - 1)
+        if (pedestals.Count == 0) return false;
+        if (currentPedestal > -1) lastPedestal = pedestals[currentPedestal];
+        currentPedestal++;
+        if (currentPedestal >= pedestals.Count)
         {
-            currentPedestal++;
-            nextPedestal = pedestals[currentPedestal];
-            return true;
+            currentPedestal = 0;
         }
-        return false;
+        nextPedestal = pedestals[currentPedestal];
+        return true;
     }
     public void Move()
     {
@@ -46,5 +74,15 @@ public class PedestalsMovement : MonoBehaviour
     private void Pause()
     {
         config.isMoving = false;
+    }
+    public void StartPressed()
+    {
+        StartCoroutine(WaitForStart());
+    }
+
+    private IEnumerator WaitForStart()
+    {
+        yield return new WaitForSeconds(startAnimation.length);
+        Move();
     }
 }
