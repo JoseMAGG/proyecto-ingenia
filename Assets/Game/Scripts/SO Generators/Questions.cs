b447ff7db85707d406d61221aa8f17aa39276e8f
JoseMagg <jose.gonzalezg1@udea.edu.co>
2023-10-25 07:58:06 -0500

    Merge with dev-semu

--- Assets/Game/Scripts/SO Generators/Questions.cs
+++ Assets/Game/Scripts/SO Generators/Questions.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu]
 public class Questions : ScriptableObject
 {
     [SerializeField] private List<string> questionList;
+    [SerializeField] private List<Sprite> questionImages;
 
     public List<string> QuestionList {  get { return questionList; } }
+    public List<Sprite>  QuestionImages { get { return questionImages; } }
 }
